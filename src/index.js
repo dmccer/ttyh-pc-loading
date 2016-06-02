a8867747745125f90b5ca74fa7bc08e413b31942
dmccer <dmccer@gmail.com>
2016-06-02 09:27:45 +0800

    time err

--- src/index.js
+++ src/index.js
@@ -7,52 +7,17 @@ import TTimers from 'ttyh-timers';
 
 @TTimers
 export default class Loading extends React.Component {
-  constructor() {
-    super();
-
-    this.state = {
-      on: false,
-      timeout: 3000
-    };
-  }
-
-  toggle(msg: string, timeout=3000: number) {
-    this.setState({
-      msg: msg,
-      timeout: timeout || 3000,
-      on: true
-    });
-
-    this.props.setTimeout(() => {
-      this.setState({
-        on: false
-      });
-    }, this.state.timeout);
-  }
-
-  show(msg: string) {
-    this.setState({
-      on: true,
-      msg: msg
-    });
-  }
-
-  close() {
-    this.setState({
-      on: false
-    });
-  }
-
   render() {
-    let loadingClassNames = cx('loading', this.state.on ? 'on' : '');
+    let { on, msg } = this.props;
+    let loadingClassNames = cx('loading', on ? 'on' : '');
 
-    return this.state.on ? (
+    return on ? (
       <div className={loadingClassNames}>
         <div className="mask"></div>
         <div className="loading-panel">
           <p className="loading-text">
             <i className="icon icon-loading"></i>
-            <span>{this.state.msg}</span>
+            <span>{msg}</span>
           </p>
         </div>
       </div>
